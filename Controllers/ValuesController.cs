bc4d01a201af78c02693f77bf84ad95c0c8534ed
unknown <jerateep_@live.com>
2019-08-19 01:28:30 +0700

    firstcommit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using demoapi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demoapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public Product Get()
+        {
+            return new Product
+            {
+                id = 1,
+                name = "Book",
+                price = 23.2
+            };
+        }
+
+        [HttpGet("{name}")]
+        public string Get(string name)
+        {
+            return "Hello : " + name;
+        }
+
+        [HttpPost]
+        public string CreateNewProduct([FromBody]Product data)
+        {
+            //Save to DB
+            return "Add " + data.name + " OK.";
+        }
+
+        [HttpPut]
+        public string UpdateProduct([FromBody]Product data)
+        {
+            // Update
+            return "Update " + data.name + " OK.";
+        }
+        
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,10 @@
+using System;
+namespace demoapi.Models
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+    }
+}
